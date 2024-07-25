89570780ee1166ceed5ebbcabd23d71e73482150
Evans Asante <asantedee@gmail.com>
2024-07-25 11:14:12 -0500

    Create a function to do the plots

--- shearandmoments.py
+++ shearandmoments.py
@@ -23,6 +23,23 @@ def lin_sf(x, a):
 def quad_sf(x, a):
     return 0.5 * (x - a) ** 2 * step_sf(x, a)
 
+def plot_graphs(l, V, M):
+    plt.subplot(211)
+    plt.plot(l, V, 'r', linewidth=1.5)
+    plt.axhline(0, color='k')
+    plt.xlabel('Distances in meters')
+    plt.ylabel('Shear Force in kN')
+    plt.title('Shear Force diagram')
+
+    plt.subplot(212)
+    plt.plot(l, M, 'r', linewidth=1.5)
+    plt.axhline(0, color='k')
+    plt.xlabel('Distances in meters')
+    plt.ylabel('Bending Moment in kNm')
+    plt.title('Bending Moment Diagram')
+
+    plt.show()
+
 def main():
     print('1. Simply supported beam/overhang')
     print('2. Cantilever')
@@ -58,26 +75,10 @@ def main():
             V = RA * step_sf(l, 0) - np.sum([P[i] * step_sf(l, x[i]) for i in range(k)], axis=0) + RB * step_sf(l, L)
             M = RA * lin_sf(l, 0) - np.sum([P[i] * lin_sf(l, x[i]) for i in range(k)], axis=0) + RB * lin_sf(l, L)
 
-            plt.subplot(211)
-            plt.plot(l, V, 'r', linewidth=1.5)
-            plt.axhline(0, color='k')
-            plt.axvline(l[-1], color='r', linewidth=1.5)
-            plt.xlabel('Distances in meters')
-            plt.ylabel('Shear Force in kN')
-            plt.title('Shear Force diagram')
-
-            plt.subplot(212)
-            plt.plot(l, M, 'r', linewidth=1.5)
-            plt.axhline(0, color='k')
-            plt.axvline(l[-1], color='r', linewidth=1.5)
-            plt.xlabel('Distances in meters')
-            plt.ylabel('Bending Moment in kNm')
-            plt.title('Bending Moment Diagram')
-
+            plot_graphs(l, V, M)
             print(f'RA={RA:.2f} kN')
             print(f'RB={RB:.2f} kN')
 
-            plt.show()
 
         elif n == 2:
             k = pyin.inputInt('Enter the number of UDLs: ', min=0)
@@ -105,26 +106,9 @@ def main():
             V = ReactionatA * step_sf(l, 0) - np.sum([w[i] * lin_sf(l, dis[i]) for i in range(k)], axis=0) + np.sum([w[i] * lin_sf(l, dis[i] + span[i]) for i in range(k)], axis=0) + ReactionatB * step_sf(l, L)
             M = ReactionatA * lin_sf(l, 0) - np.sum([w[i] * quad_sf(l, dis[i]) for i in range(k)], axis=0) + np.sum([w[i] * quad_sf(l, dis[i] + span[i]) for i in range(k)], axis=0) + ReactionatB * lin_sf(l, L)
 
-            plt.subplot(211)
-            plt.plot(l, V, 'r', linewidth=1.5)
-            plt.axhline(0, color='k')
-            plt.axvline(l[-1], color='r', linewidth=1.5)
-            plt.xlabel('Distances in meters')
-            plt.ylabel('Shear Force in kN')
-            plt.title('Shear Force diagram')
-
-            plt.subplot(212)
-            plt.plot(l, M, 'r', linewidth=1.5)
-            plt.axhline(0, color='k')
-            plt.axvline(l[-1], color='r', linewidth=1.5)
-            plt.xlabel('Distances in meters')
-            plt.ylabel('Bending Moment in kNm')
-            plt.title('Bending Moment Diagram')
-
+            plot_graphs(l, V, M)
             print(f'RA={ReactionatA:.2f} kN')
             print(f'RB={ReactionatB:.2f} kN')
-
-            plt.show()
 
         elif n == 3:
             k = pyin.inputInt('Enter the number of point loads: ', min=0)
@@ -171,26 +155,9 @@ def main():
             V = ReactionatA * step_sf(l, 0) - np.sum([P[i] * step_sf(l, x[i]) for i in range(k)], axis=0) - np.sum([w[i] * lin_sf(l, dis[i]) for i in range(c)], axis=0) + np.sum([w[i] * lin_sf(l, dis[i] + span[i]) for i in range(c)], axis=0) + ReactionatB * step_sf(l, distancebetweensupports)
             M = ReactionatA * lin_sf(l, 0) - np.sum([P[i] * lin_sf(l, x[i]) for i in range(k)], axis=0) - np.sum([w[i] * quad_sf(l, dis[i]) for i in range(c)], axis=0) + np.sum([w[i] * quad_sf(l, dis[i] + span[i]) for i in range(c)], axis=0) + ReactionatB * lin_sf(l, distancebetweensupports)
 
-            plt.subplot(211)
-            plt.plot(l, V, 'r', linewidth=1.5)
-            plt.axhline(0, color='k')
-            plt.axvline(l[-1], color='r', linewidth=1.5)
-            plt.xlabel('Distances in meters')
-            plt.ylabel('Shear Force in kN')
-            plt.title('Shear Force diagram')
-
-            plt.subplot(212)
-            plt.plot(l, M, 'r', linewidth=1.5)
-            plt.axhline(0, color='k')
-            plt.axvline(l[-1], color='r', linewidth=1.5)
-            plt.xlabel('Distances in meters')
-            plt.ylabel('Bending Moment in kNm')
-            plt.title('Bending Moment Diagram')
-
+            plot_graphs(l, V, M)
             print(f'RA={ReactionatA:.2f} kN')
             print(f'RB={ReactionatB:.2f} kN')
-
-            plt.show()
 
         elif n == 4:
             k = int(input('Enter the number of point loads: '))
@@ -258,26 +225,9 @@ def main():
             V = ReactionatA * step_sf(l, 0) - np.sum([P[i] * step_sf(l, x[i]) for i in range(k)], axis=0) - np.sum([w[i] * lin_sf(l, dis[i]) for i in range(c)], axis=0) + np.sum([w[i] * lin_sf(l, dis[i] + span[i]) for i in range(c)], axis=0) + ReactionatB * step_sf(l, distancebetweensupports)
             M = ReactionatA * lin_sf(l, 0) - np.sum([P[i] * lin_sf(l, x[i]) for i in range(k)], axis=0) - np.sum([w[i] * quad_sf(l, dis[i]) for i in range(c)], axis=0) + np.sum([w[i] * quad_sf(l, dis[i] + span[i]) for i in range(c)], axis=0) + ReactionatB * lin_sf(l, distancebetweensupports) + np.sum([concmoment[i] * step_sf(l, concdistance[i]) for i in range(conc)], axis=0)
 
-            plt.subplot(211)
-            plt.plot(l, V, 'r', linewidth=1.5)
-            plt.axhline(0, color='k')
-            plt.axvline(l[-1], color='r', linewidth=1.5)
-            plt.xlabel('Distances in meters')
-            plt.ylabel('Shear Force in kN')
-            plt.title('Shear Force diagram')
-
-            plt.subplot(212)
-            plt.plot(l, M, 'r', linewidth=1.5)
-            plt.axhline(0, color='k')
-            plt.axvline(l[-1], color='r', linewidth=1.5)
-            plt.xlabel('Distances in meters')
-            plt.ylabel('Bending Moment in kNm')
-            plt.title('Bending Moment Diagram')
-
+            plot_graphs(l, V, M)
             print(f'RA={ReactionatA:.2f} kN')
             print(f'RB={ReactionatB:.2f} kN')
-
-            plt.show()
 
     elif choice == 2:
         print('1. Cantilever with only point loads on the span')
@@ -315,26 +265,9 @@ def main():
             V = RA * step_sf(l, 0) - np.sum([P[i] * step_sf(l, x[i]) for i in range(k)], axis=0)
             M = MA * step_sf(l, 0) - RA * lin_sf(l, 0) + np.sum([P[i] * lin_sf(l, x[i]) for i in range(k)], axis=0)
 
-            plt.subplot(211)
-            plt.plot(l, V, 'r', linewidth=1.5)
-            plt.axhline(0, color='k')
-            plt.axvline(l[-1], color='r', linewidth=1.5)
-            plt.xlabel('Distances in meters')
-            plt.ylabel('Shear Force in kN')
-            plt.title('Shear Force diagram')
-
-            plt.subplot(212)
-            plt.plot(l, M, 'r', linewidth=1.5)
-            plt.axhline(0, color='k')
-            plt.axvline(l[-1], color='r', linewidth=1.5)
-            plt.xlabel('Distances in meters')
-            plt.ylabel('Bending Moment in kNm')
-            plt.title('Bending Moment Diagram')
-
+            plot_graphs(l, V, M)
             print(f'RA={RA:.2f} kN')
             print(f'MA={MA:.2f} kNm')
-
-            plt.show()
 
         elif n == 2:
             k = int(input('Enter the number of UDLs: '))
@@ -365,26 +298,9 @@ def main():
             V = RA * step_sf(l, 0) - np.sum([w[i] * lin_sf(l, dis[i]) for i in range(k)], axis=0) + np.sum([w[i] * lin_sf(l, dis[i] + span[i]) for i in range(k)], axis=0)
             M = MA * step_sf(l, 0) - RA * lin_sf(l, 0) + np.sum([w[i] * quad_sf(l, dis[i]) for i in range(k)], axis=0) - np.sum([w[i] * quad_sf(l, dis[i] + span[i]) for i in range(k)], axis=0)
 
-            plt.subplot(211)
-            plt.plot(l, V, 'r', linewidth=1.5)
-            plt.axhline(0, color='k')
-            plt.axvline(l[-1], color='r', linewidth=1.5)
-            plt.xlabel('Distances in meters')
-            plt.ylabel('Shear Force in kN')
-            plt.title('Shear Force diagram')
-
-            plt.subplot(212)
-            plt.plot(l, M, 'r', linewidth=1.5)
-            plt.axhline(0, color='k')
-            plt.axvline(l[-1], color='r', linewidth=1.5)
-            plt.xlabel('Distances in meters')
-            plt.ylabel('Bending Moment in kNm')
-            plt.title('Bending Moment Diagram')
-
+            plot_graphs(l, V, M)
             print(f'RA={RA:.2f} kN')
             print(f'MA={MA:.2f} kNm')
-
-            plt.show()
 
         elif n == 3:
             k = int(input('Enter the number of point loads: '))
@@ -426,26 +342,10 @@ def main():
             V = RA * step_sf(l, 0) - np.sum([P[i] * step_sf(l, x[i]) for i in range(k)], axis=0) - np.sum([w[i] * lin_sf(l, dis[i]) for i in range(k)], axis=0) + np.sum([w[i] * lin_sf(l, dis[i] + span[i]) for i in range(k)], axis=0)
             M = MA * step_sf(l, 0) - RA * lin_sf(l, 0) + np.sum([P[i] * lin_sf(l, x[i]) for i in range(k)], axis=0) + np.sum([w[i] * quad_sf(l, dis[i]) for i in range(k)], axis=0) - np.sum([w[i] * quad_sf(l, dis[i] + span[i]) for i in range(k)], axis=0)
 
-            plt.subplot(211)
-            plt.plot(l, V, 'r', linewidth=1.5)
-            plt.axhline(0, color='k')
-            plt.axvline(l[-1], color='r', linewidth=1.5)
-            plt.xlabel('Distances in meters')
-            plt.ylabel('Shear Force in kN')
-            plt.title('Shear Force diagram')
-
-            plt.subplot(212)
-            plt.plot(l, M, 'r', linewidth=1.5)
-            plt.axhline(0, color='k')
-            plt.axvline(l[-1], color='r', linewidth=1.5)
-            plt.xlabel('Distances in meters')
-            plt.ylabel('Bending Moment in kNm')
-            plt.title('Bending Moment Diagram')
+            plot_graphs(l, V, M)
 
             print(f'RA={RA:.2f} kN')
             print(f'MA={MA:.2f} kNm')
-
-            plt.show()
 
         elif n == 4:
             k = int(input('Enter the number of point loads: '))
@@ -503,26 +403,11 @@ def main():
             V = RA * step_sf(l, 0) - np.sum([P[i] * step_sf(l, x[i]) for i in range(k)], axis=0) - np.sum([w[i] * lin_sf(l, dis[i]) for i in range(k)], axis=0) + np.sum([w[i] * lin_sf(l, dis[i] + span[i]) for i in range(k)], axis=0)
             M = MA * step_sf(l, 0) - RA * lin_sf(l, 0) + np.sum([P[i] * lin_sf(l, x[i]) for i in range(k)], axis=0) + np.sum([w[i] * quad_sf(l, dis[i]) for i in range(k)], axis=0) - np.sum([w[i] * quad_sf(l, dis[i] + span[i]) for i in range(k)], axis=0) - np.sum([concmoment[i] * step_sf(l, concdistance[i]) for i in range(conc)], axis=0)
 
-            plt.subplot(211)
-            plt.plot(l, V, 'r', linewidth=1.5)
-            plt.axhline(0, color='k')
-            plt.axvline(l[-1], color='r', linewidth=1.5)
-            plt.xlabel('Distances in meters')
-            plt.ylabel('Shear Force in kN')
-            plt.title('Shear Force diagram')
-
-            plt.subplot(212)
-            plt.plot(l, M, 'r', linewidth=1.5)
-            plt.axhline(0, color='k')
-            plt.axvline(l[-1], color='r', linewidth=1.5)
-            plt.xlabel('Distances in meters')
-            plt.ylabel('Bending Moment in kNm')
-            plt.title('Bending Moment Diagram')
+            plot_graphs(l, V, M)
 
             print(f'RA={RA:.2f} kN')
             print(f'MA={MA:.2f} kNm')
 
-            plt.show()
 
 if __name__ == '__main__':
     main()
